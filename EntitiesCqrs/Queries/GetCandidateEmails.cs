35e84b2bb49769d470fe5d7754f540e7ae738012
andy <andy@andydote.co.uk>
2014-06-12 13:04:17 +0100

    entitiesCqrs: add queries for loading by

---
+++ EntitiesCqrs/Queries/GetCandidateEmails.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using Dapper;
+
+namespace EntitiesCqrs.Queries
+{
+	public class GetCandidateEmails
+	{
+		private readonly DbConnection _connection;
+		private readonly Candidate _parent;
+
+		public GetCandidateEmails(DbConnection connection, Candidate parent)
+		{
+			_connection = connection;
+			_parent = parent;
+		}
+
+		public IEnumerable<EmailAddress> Execute()
+		{
+			return _connection.Query<EmailAddress>(
+				"select ID, candidateID, email, isPrimary where candidateID = @candidateID",
+				new { candidateID = _parent.ID });
+		}
+	}
+}
---
+++ EntitiesCqrs/Queries/GetCandidatePhones.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using Dapper;
+
+namespace EntitiesCqrs.Queries
+{
+	public class GetCandidatePhones
+	{
+		private readonly DbConnection _connection;
+		private readonly Candidate _parent;
+
+		public GetCandidatePhones(DbConnection connection, Candidate parent)
+		{
+			_connection = connection;
+			_parent = parent;
+		}
+
+		public IEnumerable<Phone> Execute()
+		{
+			return _connection.Query<Phone>(
+				"select ID, candidateID, number, extension from phones where candidateID = @candidateID",
+				new { candidateID = _parent.ID });
+		}
+	}
+}
