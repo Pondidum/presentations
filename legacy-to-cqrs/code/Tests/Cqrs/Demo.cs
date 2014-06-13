7e1d6251e59a602eea9d41f3edf704f19e0f7bea
andy <andy@andydote.co.uk>
2014-06-13 21:29:29 +0100

    cqrs: add demo.cs

---
+++ legacy-to-cqrs/code/Tests/Cqrs/Demo.cs
@@ -0,0 +1,7 @@
+﻿namespace Tests.Cqrs
+{
+	public class Demo
+	{
+		 
+	}
+}
---
+++ legacy-to-cqrs/code/Tests/Legacy/Demo.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using EntitiesLegacy;
+
+namespace Tests.Legacy
+{
+	public class Demo
+	{
+		public void No_db_setup_done()
+		{
+			//oops forgot to setup the db...
+
+			var candidates = new CandidateCollection();
+			candidates.LoadByEmail("*@gmail.com");
+
+		}
+
+		public void Candidate_collections_mutable()
+		{
+			var candidates = new CandidateCollection();
+			candidates.LoadByEmail("*@gmail.com");
+
+			var main = candidates[0];
+			var other = candidates[1];
+
+			other.Emails.ParentID = main.ID;
+			other.Emails.Save();
+			//interesting...
+		}
+
+		public void Not_efficient()
+		{
+			var c = new Candidate();
+
+			c.Emails.Add(new EmailAddress() { Email = "one@gmail.com"});
+			c.Emails.Add(new EmailAddress() { Email = "two@gmail.com" });
+
+			c.Save();
+			//3 db connections opened and closed
+			//no transaction boundary available
+		}
+	}
+}
