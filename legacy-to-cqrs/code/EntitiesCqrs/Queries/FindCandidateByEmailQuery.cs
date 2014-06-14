f7364beebc6cc1b6ec13a13b5df4002c0769bd71
andy <andy@andydote.co.uk>
2014-06-14 14:56:11 +0100

    cqrs: add findCandidateByEmailQuery

---
+++ legacy-to-cqrs/code/EntitiesCqrs/Queries/FindCandidateByEmailQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using Dapper;
+using EntitiesCqrs.Infrastructure;
+
+namespace EntitiesCqrs.Queries
+{
+	public class FindCandidateByEmailQuery
+	{
+		private readonly DbConnection _connection;
+		private readonly string _email;
+
+		public FindCandidateByEmailQuery(DbConnection connection, string email)
+		{
+			_connection = connection;
+			_email = email;
+		}
+
+		public IEnumerable<Candidate> Execute()
+		{
+			var arg = new { email = "%" + _email + "%" };
+
+			return _connection
+				.Query<Candidate>(
+					"select ID, Name, Dob, Sex " +
+					"from candidates " + 
+					"join emails on e.parentID = c.ID " +
+					"where e.email like @email", arg)
+				.Apply(candidate => candidate.Emails.AddRange(new GetCandidateEmails(_connection, candidate).Execute()))
+				.Apply(candidate => candidate.Phones.AddRange(new GetCandidatePhones(_connection, candidate).Execute()));
+		}
+	}
+}
