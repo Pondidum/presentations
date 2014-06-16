8ebec28bf64fa375bf05c6db821d203f17427fe6
andy <andy@andydote.co.uk>
2014-06-16 20:52:39 +0100

    cqrs: add commandHandlerRegistry

---
+++ legacy-to-cqrs/code/EntitiesCqrsCommandHandler/CommandHandlerRegistry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EntitiesCqrsCommandHandler.Infrastructure;
+
+namespace EntitiesCqrsCommandHandler
+{
+	public class CommandHandlerRegistry
+	{
+		public IEnumerable<Type> Commands { get; private set; }
+		public IEnumerable<KeyValuePair<Type, IList<Type>>> CommandHandlers { get; private set; }
+
+		public CommandHandlerRegistry()
+		{
+			Commands = GetCommands();
+			CommandHandlers = GetCommandHandlers();
+		}
+
+		private IEnumerable<KeyValuePair<Type, IList<Type>>> GetCommandHandlers()
+		{
+			IDictionary<Type, IList<Type>> handlers = new Dictionary<Type, IList<Type>>();
+
+			typeof(ICommandHandler<>)
+				.Assembly
+				.GetExportedTypes()
+				.Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<>)))
+				.Each(t => Add(handlers, t));
+			return handlers;
+		}
+
+		private IEnumerable<Type> GetCommands()
+		{
+			return typeof(ICommand)
+				.Assembly
+				.GetExportedTypes()
+				.Where(x => x.GetInterfaces().Contains(typeof(ICommand)))
+				.ToList();
+		}
+
+		private void Add(IDictionary<Type, IList<Type>> commands, Type type)
+		{
+			var command = type
+				.GetInterfaces()
+				.First(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(ICommandHandler<>))
+				.GetGenericArguments()
+				.First();
+
+			if (commands.ContainsKey(command) == false)
+			{
+				commands[command] = new List<Type>();
+			}
+
+			commands[command].Add(type);
+		}
+	}
+}
