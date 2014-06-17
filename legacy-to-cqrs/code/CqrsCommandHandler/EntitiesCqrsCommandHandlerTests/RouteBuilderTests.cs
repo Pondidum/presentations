804d4f86a6300b0c67bf3a04f0e43a06c2b7c7d6
andy <andy@andydote.co.uk>
2014-06-17 20:26:04 +0100

    cqrs: copy and restructure code to separate folders

---
+++ legacy-to-cqrs/code/CqrsCommandHandler/EntitiesCqrsCommandHandlerTests/RouteBuilderTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using EntitiesCqrsCommandHandler;
+using EntitiesCqrsCommandHandler.Commands;
+using EntitiesCqrsCommandHandler.Entities;
+using StructureMap;
+using Xunit;
+
+namespace EntitiesCqrsCommandHandlerTests
+{
+	public class RouteBuilderTests
+	{
+		[Fact]
+		public void When_creating_all_routes()
+		{
+			var container = new Container(config =>
+			{
+				config.Scan(scanner =>
+				{
+					scanner.AssemblyContainingType<ICommand>();
+					scanner.WithDefaultConventions();
+				});
+
+				config.For<IDbConnection>().Use(() => new SqlConnection());
+			});
+
+			var bus = new InMemoryBus();
+			var builder = new RouteBuilder(container, bus, new CommandHandlerRegistry());
+
+			bus.Publish(new SaveCandidateCommand(new Candidate()));
+
+		} 
+	}
+}
---
+++ legacy-to-cqrs/code/Legacy/Tests/Cqrs/Demo.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using EntitiesCqrs;
+using EntitiesCqrs.Commands;
+using EntitiesCqrs.Queries;
+
+namespace Tests.Cqrs
+{
+	public class Demo
+	{
+		public void Forces_db_connection()
+		{
+			var connection = new SqlConnection();
+			var query = new FindCandidateByNameQuery(connection, "*@gmail.com");
+
+			var candidates = query.Execute();
+		}
+
+		public void Has_transaction_boundaries()
+		{
+			var c = new Candidate();
+
+			c.Emails.Add(new EmailAddress { Email = "one@gmail.com" });
+			c.Emails.Add(new EmailAddress { Email = "two@gmail.com" });
+
+			using (var connection = new SqlConnection())
+			{
+				using (var tx = connection.BeginTransaction())
+				{
+					var command = new SaveCandidateCommand(connection, c);
+					command.Execute();
+
+					tx.Commit();
+				}
+			}
+		}
+	}
+}
