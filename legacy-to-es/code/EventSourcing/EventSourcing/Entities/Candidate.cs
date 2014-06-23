87dcf43b4613c18681f0b94263bbb556be08d730
andy <andy@andydote.co.uk>
2014-06-23 20:25:01 +0100

    eventsourcing: add a couple of candidate based events

--- legacy-to-es/code/EventSourcing/EventSourcing/Entities/Candidate.cs
+++ legacy-to-es/code/EventSourcing/EventSourcing/Entities/Candidate.cs
@@ -1,21 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using EventSourcing.Events;
+using EventSourcing.Infrastructure;
 
 namespace EventSourcing.Entities
 {
-	public class Candidate
+	public class Candidate : DomainObject
 	{
+		private readonly List<EmailAddress> _emails;
+		private readonly List<Phone> _phones;
+
 		public string Name { get; set; }
 		public DateTime DoB { get; set; }
 		public Sexes Sex { get; set; }
 
-		public List<EmailAddress> Emails { get; private set; }
-		public List<Phone> Phones { get; private set; }
+		public IEnumerable<EmailAddress> Emails { get { return _emails; } }
+		public IEnumerable<Phone> Phones { get { return _phones; } }
 
 		public Candidate()
 		{
-			Emails = new List<EmailAddress>();
-			Phones = new List<Phone>();
+			_emails = new List<EmailAddress>();
+			_phones = new List<Phone>();
+		}
+
+		public static Candidate Create(string name, DateTime dob)
+		{
+			var candidate = new Candidate();
+			candidate.ApplyEvent(new CreateCandidateEvent(name, dob));
+
+			return candidate;
+		}
+
+		public void AddPhone(string number, string extension)
+		{
+			ApplyEvent(new AddPhoneEvent(number, extension));
+		}
+
+
+		private void OnCreateCandidate(CreateCandidateEvent domainEvent)
+		{
+			ID = domainEvent.CandidateID;
+			Name = domainEvent.Name;
+			DoB = domainEvent.DoB;
+		}
+
+		private void OnAddPhone(AddPhoneEvent domainEvent)
+		{
+			_phones.Add(new Phone
+			{
+				Number = domainEvent.Number, 
+				Extension = domainEvent.Extension
+			});
 		}
 	}
 }
---
+++ legacy-to-es/code/EventSourcing/EventSourcing/Events/CreateCandidateEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EventSourcing.Infrastructure;
+
+namespace EventSourcing.Events
+{
+	public class CreateCandidateEvent : DomainEvent
+	{
+		public Guid CandidateID { get; private set; }
+		public string Name { get; private set; }
+		public DateTime DoB { get; private set; }
+
+		public CreateCandidateEvent(string name, DateTime dob)
+		{
+			CandidateID = Guid.NewGuid();
+			Name = name;
+			DoB = dob;
+		}
+	}
+}
